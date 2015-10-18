a6db997eef0dd8f50a726961887f74dd6c9fbb28
captainmoha <admiralmoha@gmail.com>
2015-10-17 21:43:12 -0400

    Add first six excerises' code

---
+++ ex1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(int argc, char *argv[]) 
+{
+    puts("Hello world.");
+    puts("This is C");
+    puts("Yes this is dog.");
+    
+    return 0;
+}
---
+++ ex3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int age = 21;
+    int height = 170;
+    printf("I am %d years old. \n", age);
+    printf("I am %d cm tall. \n", height);
+    return 0;
+}
